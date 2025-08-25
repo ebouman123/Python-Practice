0354365d47f8db9621b5b5cd85c17b1e4200148a
Evan Bouman <evan.bouman@gmail.com>
2025-08-25 15:26:37 -0500

    String methods

---
+++ python_practice_2.py
@@ -0,0 +1,18 @@
+# the "in" keyword can be used to check to see if one string is in another string (returns boolean)
+
+fruit = "banana"
+print("n" in fruit) # True
+print("m" in fruit) # False
+print("nan" in fruit) # True
+
+if "a" in fruit:
+    print("Found it!") # "Found it!"
+    
+# String Library (methods)
+
+greet = "Hello Gordon"
+lowercaseGreet = greet.lower()
+print(lowercaseGreet) # "hello gordon"
+print(greet) # "Hello Gordon"
+
+
