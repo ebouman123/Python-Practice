61fe1992c2ddcc45729fc65ad6172c3113828f77
Evan Bouman <evan.bouman@gmail.com>
2025-08-25 15:13:08 -0500

    is, is not

--- python_practice.py
+++ python_practice.py
@@ -6,3 +6,14 @@ for i in [3, 41, 12, 9, 74, 15]:
     print("Loop:", i, smallest)
 print("Smallest:", smallest)
 
+# is and is not are similar to, but stronger than ==
+
+smallest = None
+print("Before")
+for value in [3, 41, 12, 9, 74, 15]:
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print (smallest, value)
+print("After", smallest)
