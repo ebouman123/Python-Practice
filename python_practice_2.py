1f4776923ea9e22c5b8cf15759213608b9e2b4bb
Evan Bouman <evan.bouman@gmail.com>
2025-08-26 15:24:56 -0500

    new line

--- python_practice_2.py
+++ python_practice_2.py
@@ -15,4 +15,16 @@ lowercaseGreet = greet.lower()
 print(lowercaseGreet) # "hello gordon"
 print(greet) # "Hello Gordon"
 
+i = greet.find("lo")
+print(i)
+
+# open() can be used to read files
+
+#fhand = open("stuff.txt")
+
+# \n can be used to start a new line, these are at the end of every lines in text files
+
+print("Hello\nWorld!")
+# Hello
+# World!
 
